09d3c67abced6d4dcc79749d998f4a80391e4283
cyrus <cryrus1999coding@gmail.com>
2026-07-07 22:23:03 +0330

    Incrementing and Pre-Incrementing

--- Program.cs
+++ Program.cs
@@ -1,63 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿int num = 0;
 
-string question1 = "What is capital in Germany";
-string answer1 = "Berlin";
 
-string question2 = "What is 2 plus 2";
-string answer2 = "4";
+Console.WriteLine($"Num is {num}");
 
-string question3 = "What color do you get by mixing blue and yellow";
-string answer3 = "green";
+// Inceremnting of int 
+num ++ ;
+Console.WriteLine($"Num is {num}");
 
-int score = 0;
+// Increment before using the variable ( Pre-Incrementing)
+Console.WriteLine("Num is {0}", ++num);
 
-Console.WriteLine(question1);
-string userAnswer1 = Console.ReadLine();
-if (userAnswer1.Trim().ToLower() == answer1.ToLower())
-{
-    Console.WriteLine("Correct");
-    score++;
-}
-else
-{
-    Console.WriteLine("Wrong, the correct ansser is: " + answer1);
-}
+Console.WriteLine("Num is {0}", num++);
+Console.WriteLine("Num is {0}", num);
 
-Console.WriteLine(question2);
-string userAnswer2 = Console.ReadLine();
-if (userAnswer2.Trim().ToLower() == answer2.ToLower())
-{
-    Console.WriteLine("Correct");
-    score++;
-}
-else
-{
-    Console.WriteLine("Wrong, the correct anser is: " + answer2);
-}
-Console.WriteLine(question3);
-string userAnswer3 = Console.ReadLine();
-if (userAnswer3.Trim().ToLower() == answer3.ToLower())
-{
-    Console.WriteLine("Correct");
-    score++;
-}
-else
-{
-    Console.WriteLine("Wrong, the correct anser is: " + answer3);
-}
-
-if (score == 3)
-{
-    Console.WriteLine("You guessed all Right congratulations !");
-}
-else if (score > 0)
-{
-
-    Console.WriteLine("Try again and see if see get some answer right next time");
-}
-else
-{
-    Console.WriteLine("Damn you are retarded");
-}
 
 Console.ReadKey();
---
+++ docs/Incrementing-and-Pre-Incrementing.md
@@ -0,0 +1,105 @@
+﻿# Incrementing and Pre-Incrementing
+
+we're going to see some more operators
+
+```cs
+int num = 0;
+
+
+Console.WriteLine($"Num is{num}");
+// Inceremnting of int 
+num ++ ;
+Console.WriteLine($"Num is{num}");
+
+
+Console.ReadKey();
+```
+
+```console
+Num is 0
+Num is 1
+```
+
+🔑 It does matter if we're incrementing before or after the variable name 
+
+```cs
+int num = 0;
+
+
+Console.WriteLine($"Num is {num}");
+
+// Inceremnting of int 
+num ++ ;
+Console.WriteLine($"Num is {num}");
+
+// Increment before using the variable
+Console.WriteLine("Num is {0}", ++num); 👈
+
+
+Console.ReadKey();
+```
+
+```console
+Num is 0
+Num is 1
+Num is 2 👈
+```
+
+```cs
+int num = 0;
+
+
+Console.WriteLine($"Num is {num}");
+
+// Inceremnting of int 
+num ++ ;
+Console.WriteLine($"Num is {num}");
+
+// Increment before using the variable
+Console.WriteLine("Num is {0}", ++num);
+Console.WriteLine("Num is {0}", num++);
+
+
+Console.ReadKey();
+```
+
+```console
+Num is 0
+Num is 1
+Num is 2 👈
+Num is 2 👈
+```
+
+```cs
+int num = 0;
+
+
+Console.WriteLine($"Num is {num}");
+
+// Inceremnting of int 
+num ++ ;
+Console.WriteLine($"Num is {num}");
+
+// Increment before using the variable
+Console.WriteLine("Num is {0}", ++num); 👈
+Console.WriteLine("Num is {0}", num++); 👈
+Console.WriteLine("Num is {0}", num); 👈
+
+
+Console.ReadKey();
+```
+
+
+```console
+Num is 0
+Num is 1
+Num is 2 👈
+Num is 2 👈
+Num is 3 👈
+```
+
+- `++num` :  
+  Called `Pre-Incrementing`
+
+🔑 Same thing works for De-crementing
+  
